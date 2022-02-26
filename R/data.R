12673b7d3d044b2f85a51cd4ff90030bd2b03ac8
FrankLef <flefebvre01@hotmail.com>
2022-02-26 13:41:19 -0500

    data

---
+++ R/data.R
@@ -0,0 +1,92 @@
+#' Admissions data from the NCES IPEDS 2018-2019 provisionally.
+#'
+#' See section 1.2.2 for details. Admissions data collected in the Fall 2018.
+#'
+#' @docType data
+#'
+#' @format Dataframe with 1217 rows and 3 variables.
+#' \describe{
+#'   \item{selective}{1 = institution admitted less than 50% of applicants}
+#'   \item{female}{1 = more than 69% of students admitted were women}
+#'   \item{highmathsat}{1 = average of the 25th and 75th precentiles of SAT was
+#'   higher than 600}
+#' }
+#'
+#' @source \url{nces.ed.gov}
+#'
+"nces"
+
+#' What-If study (Cook et al (2019))
+#'
+#' See section 1.2.3.1 for details.
+#'
+#' @docType data
+#'
+#' @format Dataframe with 165 rows and 4 variables.
+#' \describe{
+#'   \item{T}{1 = naltrexone, 0 = placebo}
+#'   \item{A}{1 = reduced drinking for prior 30 days}
+#'   \item{H}{1 = unsuppresed HIV load}
+#'   \item{Y}{1 = unsuppresed HIV load at month 4}
+#' }
+#'
+"whatifdat"
+
+#' Double What-If study Simulation
+#'
+#' See section 1.2.3.2 for details.
+#'
+#' @docType data
+#'
+#' @format Dataframe with 1000 rows and 6 variables.
+#' \describe{
+#'   \item{AD0}{HIV antiretroviral adherence at time 0}
+#'   \item{VL0}{Viral load at time 1}
+#'   \item{T}{1 = naltrexone}
+#'   \item{A}{1 = reduced drinking}
+#'   \item{AD1}{HIV antiretroviral adherence at time 0}
+#'   \item{VL1}{Viral load at time 1}
+#' }
+#'
+"doublewhatifdat"
+
+#' Dataset from 2018 GSS
+#'
+#' See section 1.2.4. Data on 2348 surveyed individuals.
+#'
+#' @docType data
+#'
+#' @format Dataframe with 2348 rows and 12 variables.
+#' \describe{
+#'   \item{age}{todo}
+#'   \item{gt65}{todo}
+#'   \item{attend}{todo}
+#'   \item{gthsedu}{todo}
+#'   \item{magthsedu}{todo}
+#'   \item{pagthsedu}{todo}
+#'   \item{fair}{todo}
+#'   \item{owngun}{todo}
+#'   \item{conservative}{todo}
+#'   \item{trump}{todo}
+#'   \item{white}{todo}
+#'   \item{female}{todo}
+#' }
+#' an exported object
+#'
+#' @source \url{www.gss.norc.org}
+"gss"
+
+#' Children's Oncology Group from Robins 1989
+#'
+#' See section 1.2.5. Data from Children's Oncology Group.
+#'
+#' @docType data
+#'
+#' @format Dataframe with 1190 rows and 4 variables.
+#' \describe{
+#'   \item{A1}{1 = experimental therapy, 0 = standard therapy}
+#'   \item{H2}{1 = response data is available}
+#'   \item{A2}{1 = patient received bone marrow transplant}
+#'   \item{Y}{1 = patient surviving at two years}
+#' }
+"cogdat"
