75dbaec9274ab63670e0539998ab80bc906f75a7
FrankLef <flefebvre01@hotmail.com>
2022-03-22 07:03:32 -0400

    ggp_measures

--- R/ggp_measures.R
+++ R/ggp_measures.R
@@ -8,48 +8,33 @@
 #' @param data Data frame with effect measures.
 #' @param title Title.
 #' @param subtitle Subtitle.
+#' @param digits Number of digits to round off.
 #'
-#' @importFrom rlang .data
-#' @importFrom dplyr bind_rows
 #' @importFrom ggplot2 element_text
 #' @importFrom ggrepel geom_text_repel
 #'
-#' @return A ggplot object.•
+#' @return A ggplot object.
 #' @export
-ggp_measures <- function(data, title = "Title", subtitle = "Subtitle") {
-  the_names <- c("RD", "RR", "RR*", "OR")
-  stopifnot(all(the_names %in% data$name))
+ggp_measures <- function(data, title = "Title", subtitle = "Subtitle",
+                         digits = 2) {
 
-  conf <- data$conf[1]
+  df <- ggp_measures_df(data)
+  conf <- df$conf[1]
 
-  pdf1 <- data %>%
-    filter(.data[["name"]] %in% the_names)
-  pdf2 <- data.frame(
-    name = c("AF\U2020", "CP\U2020"),
-    est = c(1 - 1 / data$est[data$name == "RR"],
-            1 - 1 / data$est[data$name == "RR*"]),
-    conf = conf,
-    lci = c(1 - 1 / data$lci[data$name == "RR"],
-            1 - 1 / data$lci[data$name == "RR*"]),
-    uci = c(1 - 1 / data$uci[data$name == "RR"],
-            1 - 1 / data$uci[data$name == "RR*"]))
-  pdf <- bind_rows(pdf1, pdf2) %>%
-    mutate(group = c("zero", "one", "one", "one", "zero", "zero"))
-
-  ggplot(pdf, aes(x = .data[["est"]], xmin = .data[["lci"]],
+  ggplot(df, aes(x = .data[["est"]], xmin = .data[["lci"]],
                   xmax = .data[["uci"]], y = .data[["name"]] ,
                   color = .data[["group"]])) +
-    ggplot2::geom_pointrange(aes(color = .data[["group"]]), fatten = 3, size = 1) +
     ggplot2::geom_vline(xintercept = c(0, 1), color = c("lightseagreen", "violet"),
-               linetype = "longdash", size = 1) +
+               linetype = "solid", size = 4, alpha = 0.5) +
+    ggplot2::geom_pointrange(aes(color = .data[["group"]]), fatten = 3, size = 1) +
     ggrepel::geom_text_repel(aes(x = .data[["est"]], y = .data[["name"]],
-                                 label = round(.data[["est"]], 2)),
+                                 label = round(.data[["est"]], digits)),
                              size = 4, color = "navy") +
     ggrepel::geom_text_repel(aes(x = .data[["lci"]], y = .data[["name"]],
-                                 label = round(.data[["lci"]], 2)),
+                                 label = round(.data[["lci"]], digits)),
                              size = 4, color = "navy") +
     ggrepel::geom_text_repel(aes(x = .data[["uci"]], y = .data[["name"]],
-                                 label = round(.data[["uci"]], 2)),
+                                 label = round(.data[["uci"]], digits)),
                              size = 4, color = "navy") +
     ggplot2::scale_x_continuous(breaks = c(0, 1)) +
     ggplot2::scale_color_manual(values = c("zero" = "darkgreen", "one" = "magenta"),
@@ -60,4 +45,40 @@ ggp_measures <- function(data, title = "Title", subtitle = "Subtitle") {
          subtitle = subtitle,
          caption = "\U2020 Assuming the treatment cannot harm anyone.",
          x = NULL, y = NULL)
+}
+
+#' Create the data fame for \code{ggp_measures}
+#'
+#' @param data Data frame to plot.
+#'
+#' @importFrom rlang .data
+#' @importFrom dplyr bind_rows
+#'
+#' @seealso ggp_measures
+#'
+#' @return Data frame.
+ggp_measures_df <- function(data) {
+  the_names <- c("RD", "RR", "RR*", "OR")
+  stopifnot(all(the_names %in% data$name))
+
+  # filter and order the data
+  pos <- match(the_names, data$name, nomatch = 0L)
+  df1 <- data[pos, ]
+
+  conf <- df1$conf[1]
+
+  # pdf1 <- data %>%
+  #   filter(.data[["name"]] %in% the_names)
+  df2 <- data.frame(
+    name = c("AF\U2020", "CP\U2020"),
+    est = c(1 - 1 / df1$est[df1$name == "RR"],
+            1 - 1 / df1$est[df1$name == "RR*"]),
+    conf = conf,
+    lci = c(1 - 1 / df1$lci[df1$name == "RR"],
+            1 - 1 / df1$lci[df1$name == "RR*"]),
+    uci = c(1 - 1 / df1$uci[df1$name == "RR"],
+            1 - 1 / df1$uci[df1$name == "RR*"]))
+  df <- bind_rows(df1, df2) %>%
+    mutate(group = c("zero", "one", "one", "one", "zero", "zero"))
+  df
 }
--- tests/testthat/test-ggp_measures.R
+++ tests/testthat/test-ggp_measures.R
@@ -1,3 +1,18 @@
+test_that("ggp_measures_df", {
+  df <- data.frame(
+    name = c("EY0", "EY1", "RD", "RR", "RR*", "OR"),
+    conf = 0.95,
+    est = c(0.375, 0.289, -0.086, 0.770, 0.879, 0.677),
+    lci = c(0.262, 0.207, -0.206, 0.538, 0.729, 0.394),
+    uci = c(0.485, 0.0371, 0.036, 1.109, 1.069, 1.179)
+  )
+  out <- ggp_measures_df(df)
+
+
+  expect_identical(dim(out), c(6L, 6L))
+})
+
+
 test_that("ggp_measures", {
   df <- data.frame(
     name = c("EY0", "EY1", "RD", "RR", "RR*", "OR"),
